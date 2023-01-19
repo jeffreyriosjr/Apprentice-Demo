dcfe0a277e8508d6f585426fd8e96a6a8121a66a
unknown <eddiemoreno@gmail.com>
2023-01-19 14:22:38 -0800

    testing

--- homepage.php
+++ homepage.php
@@ -11,7 +11,7 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-        hello eddie
+        Hello Jeff
        <img src="images/sample.png" alt="">
 		<?php
 		while ( have_posts() ) :
